cbdce4eabacafc3583a8640ae799349bf6a2660b
Wojciech Szczurek <szczurek.wojciech@gmail.com>
2020-05-13 08:29:48 -0500

    feat: aws

--- config/scout_elastic.php
+++ config/scout_elastic.php
@@ -2,10 +2,23 @@
 
 return [
     'client' => [
-        'hosts' => [
-            env('SCOUT_ELASTIC_HOST', 'localhost:9200'),
-        ],
+        'hosts' =>  [
+            [
+                'host'       => env('ELASTICSEARCH_HOST', 'localhost'),
+                'port'       => env('ELASTICSEARCH_PORT', 9200),
+                'scheme'     => env('ELASTICSEARCH_SCHEME', null),
+                'user'       => env('ELASTICSEARCH_USER', null),
+                'pass'       => env('ELASTICSEARCH_PASS', null),
+
+                // If you are connecting to an Elasticsearch instance on AWS, you will need these values as well
+                'aws'        => env('AWS_ELASTICSEARCH_ENABLED', true),
+                'aws_region' => env('AWS_DEFAULT_REGION', ''),
+                'aws_key'    => env('AWS_ACCESS_KEY_ID', ''),
+                'aws_secret' => env('AWS_SECRET_ACCESS_KEY', '')
+            ],
+        ]
     ],
+
     'update_mapping' => env('SCOUT_ELASTIC_UPDATE_MAPPING', true),
     'indexer' => env('SCOUT_ELASTIC_INDEXER', 'single'),
     'document_refresh' => env('SCOUT_ELASTIC_DOCUMENT_REFRESH'),
